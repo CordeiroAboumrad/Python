4cf833b1085207f8d9e32b5c2570c477f4b78bf6
jean.aboumrad <cordeiro.aboumrad@gmail.com>
2021-01-16 18:01:11 -0300

    Addition of the project Silent Auction.

---
+++ Silent Auction/main.py
@@ -0,0 +1,27 @@
+from art import logo
+from clear import clear
+
+print(logo)
+print("\n\nHello!\n")
+
+
+execute = 'y'
+bid_dictionary = {}
+
+while(execute == 'y'):
+    name = str(input("What's your name?\n"))
+    bid = float(input("What's your bid?\n"))
+    
+    bid_dictionary[name] = bid
+
+    execute = str(input("Do you wish to add a new bid (y/n)?\n"))
+    clear()
+
+biggest = 0
+biggest_key = ""
+
+for key in bid_dictionary:
+    if bid_dictionary[key] > biggest:
+        biggest_key = key
+
+print(f"The winner of the silent auction is {biggest_key}, with the bid of {bid_dictionary[biggest_key]}.")
