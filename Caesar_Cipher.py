09ca76e7902b96558dc4e1243a42292c1c9413e0
jean.aboumrad <cordeiro.aboumrad@gmail.com>
2021-01-16 15:58:33 -0300

    Fixed cases when characters added are not in the alphabet list.

--- Caesar_Cipher.py
+++ Caesar_Cipher.py
@@ -1,20 +1,26 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+
 
 def caesar(start_text, shift_amount, cipher_direction):
   end_text=""
   if cipher_direction == "decode":
     shift_amount *= -1
-  for letter in start_text:
-    position = alphabet.index(letter)
-    print(f"The old position was {position}.")
-    new_position = position + shift_amount
-    print(f"The new position is {new_position % len(alphabet)}.")
-    end_text += alphabet[new_position % len(alphabet)]
+  for char in start_text:
+    if char in alphabet:
+      position = alphabet.index(char)
+      new_position = position + shift_amount
+      end_text += alphabet[new_position % len(alphabet)]
+    else:
+      end_text += char
+
   print(f"The {cipher_direction}d text is {end_text}.")
 
+while(str(input("Do you wish to continue with this program? (y/n)")) == 'y'):
+  direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+  text = input("Type your message:\n").lower()
+  shift = int(input("Type the shift number:\n"))
 
-caesar(text, shift, direction)
+  caesar(text, shift, direction)
+
+print("Thanks for using this program. Goodbye.")
