4a8e55fba4ed9b3093866dbe4115de85dcf3546f
CordeiroAboumrad <cordeiro.aboumrad@gmail.com>
2021-03-10 18:40:47 -0300

    Password Manager and NATO Alphabet projects improved, day-30 file added.

--- NATO-Alphabet/main.py
+++ NATO-Alphabet/main.py
@@ -5,6 +5,14 @@ data = pandas.read_csv("nato_phonetic_alphabet.csv")
 data_dictionary = {row.letter: row.code for (index, row) in data.iterrows()}
 # print(data_dictionary)
 
-word = input("Type the word desired for the NATO conversion: ")
-word_to_NATO = [data_dictionary[letter.upper()] for letter in word]
-print(word_to_NATO)
+is_wrong = True
+while is_wrong:
+    try:
+        word = input("Type the word desired for the NATO conversion: ")
+        word_to_NATO = [data_dictionary[letter.upper()] for letter in word]
+    except KeyError:
+        print("Sorry, only letters are allowed for the conversion.")
+    else:
+        print(word_to_NATO)
+        print("Application successful.")
+        is_wrong = False
---
+++ day-30/main.py
@@ -0,0 +1,39 @@
+# # File Not Found
+#
+# try:
+#     file = open("a_file.txt")
+#     a_dictionary = {"key": "value"}
+#     print(a_dictionary["key"])
+# except FileNotFoundError:
+#     file = open("a_file.txt", "w")
+#     file.write("Something")
+# except KeyError as error_message:
+#     print(f"The key {error_message} does not exist.")
+# else:
+#     content = file.read()
+#     print(content)
+# finally:
+#     file.close()
+#     print("File closed")
+#     raise TypeError("This is an error I made up")
+#
+# # KeyError
+# # a_dictionary = {"key": "value"}
+# # value = a_dictionary["non_existent_key"]
+#
+# # IndexError
+# # fruits_list = ["Apple", "Banana", "Pear"]
+# # fruit = fruits_list[3]
+#
+# # TypeError
+# # text = "abc"
+# # print(text + 5)
+
+height = float(input("Height: "))
+weight = float(input("Weight: "))
+
+if height > 3:
+    raise ValueError("Human height should not be over 3 meters")
+
+bmi = weight / height ** 2
+print(bmi)
